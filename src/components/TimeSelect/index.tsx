f268ef0e43edcadff4a235df85bf17f61fcf68d6
DESKTOP-RL3KVOI\杨旭东 <496689176@qq.com>
2018-01-29 15:26:12 +0800

    DatePicker (时间范围选择)

---
+++ src/components/TimeSelect/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
+
+export interface TimeSelectProps {
+
+}
+
+export default class TimeSelect extends React.PureComponent<TimeSelectProps, any>{
+    constructor(props) {
+        super(props);
+        this.state = {
+            longTime: []
+        };
+    }
+
+    onChange(value, dateString) {
+        const { longTime } = this.state;
+        this.setState({
+            longTime: [value[0]._d.getTime(), value[1]._d.getTime()]
+        })
+    }
+
+    getTimeForMs() {
+        const { longTime } = this.state;
+        return longTime;
+    }
+
+    onOk(value) {
+        console.log('onOk: ', value);
+    }
+
+    render() {
+        return (
+            <div>
+                <RangePicker
+                    showTime={{ format: 'YYYY-MM-DD HH:mm' }}
+                    format="YYYY-MM-DD HH:mm:ss"
+                    placeholder={['Start Time', 'End Time']}
+                    onChange={this.onChange}
+                    onOk={this.onOk}
+                />
+            </div>
+        )
+    }
+}
