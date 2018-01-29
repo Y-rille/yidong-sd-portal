5aab15086cfcd5d81f88515bb944ee8b128d4edd
王曼玉 <manyu.wang@hpe.com>
2018-01-29 18:24:28 +0800

    仪表盘

--- src/components/InstrumentPanel/index.tsx
+++ src/components/InstrumentPanel/index.tsx
@@ -27,8 +27,6 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
         super(props);
         this.state = {
         };
-
-        
     }
     componentDidMount() {
         var options = {
@@ -39,12 +37,12 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
             pane: { // 仪表盘
                 center: ['50%', '85%'],
                 size: '100%',
-                startAngle: -90,//  起始角度
+                startAngle: -90, //  起始角度
                 endAngle: 90,
                 background: {
                     backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
                     innerRadius: '60%', // 刻度內轴位置，厚度
-                    outerRadius: '100%',// 刻度外轴
+                    outerRadius: '100%', // 刻度外轴
                     shape: 'arc'
                 }
             },
@@ -55,7 +53,7 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
                 enabled: false
             },
             yAxis: {
-                stops: [    //进度条颜色，从0-1
+                stops: [ // 进度条颜色，从0-1
                     [0.3, 'yellow'], // green
                     [0.5, '#DDDF0D'], // yellow
                     [0.8, '#DF5353'] // red
@@ -65,27 +63,24 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
                 tickPixelInterval: 400,
                 tickWidth: 0,
                 title: {
-                    text:'title',
+                    text: 'title',
                     y: -75 // 标题高度
                 },
                 labels: {
                     y: 14 // 刻度范围位置
                 }
-            },
-            
-            plotOptions: {  //中间显示框
+            },          
+            plotOptions: { // 中间显示框
                 solidgauge: {
                     dataLabels: {
                         y: 15,
                         borderWidth: 0,
                         useHTML: true
-                    }
-                  
-                }
-                
+                    }                 
+                }           
             }
         }
-        let chose_options={
+        let chose_options = {
             yAxis: {
                 min: 0,
                 max: 200,
@@ -113,14 +108,14 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
                     format: '<div style="text-align:center"><span style="font-size:25px;color:' +
                     ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
                     '<span style="font-size:12px;color:silver">km/h</span></div>',
-                    enabled:false
+                    enabled: false
                 },
                 tooltip: {
                     valueSuffix: ' km/h'
                 }
             }]
         }
-        options=merge({}, options, chose_options)
+        options = merge({}, options, chose_options)
         
         var chart = Highcharts.chart(this.container, options, function (_chart) {
            // if (!_chart.renderer.forExport) {
@@ -134,7 +129,7 @@ export default class InstrumentPanel extends React.PureComponent<InstrumentPanel
                 //     }
                 //     point.update(newVal);
                 // }, 3000);
-            //}
+            // }
         });
     }
 
