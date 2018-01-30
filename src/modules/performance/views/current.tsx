b791c56b8b0cfab97ba264f92cb3df07d70155b0
王曼玉 <manyu.wang@hpe.com>
2018-01-30 11:31:51 +0800

    仪表盘export，基本参数设置

--- src/modules/performance/views/current.tsx
+++ src/modules/performance/views/current.tsx
@@ -6,20 +6,35 @@ import {
     Route, Link
 } from 'react-router-dom'
 
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 
 import InstrumentPanel from '../../../components/InstrumentPanel'
 import LineChart from '../../../components/LineChart'
 
 class Home extends React.Component<any, any> {
+    instrumentPanel_1: any
     tabClick() {
 
+    }
+    printInstrumentPane() {
+        this.instrumentPanel_1.chartExport()
     }
     render() {
         return (
             <Row gutter={20} style={{ padding: '0 20px' }}>
                 <Col className="gutter-row" span={12}>
-                    <div className="gutter-box"><InstrumentPanel /></div>
+                    <div className="gutter-box">
+                        仪表盘<Button onClick={this.printInstrumentPane.bind(this)} type="primary" size="small" shape="circle" icon="download" style={{ float: 'right' }}></Button>
+                        <InstrumentPanel
+                            ref={(node) => { this.instrumentPanel_1 = node }}
+                            data={{
+                                title: '速度',
+                                min: 0,
+                                max: 200,
+                                current: 100
+                            }}
+                        />
+                    </div>
                 </Col>
                 <Col className="gutter-row" span={12}>
                     <div className="gutter-box"><LineChart /></div>
