c64d61d73f5f6151542f8dd612077777cc4be76e
lijie <jiel@kailuzhe.cn>
2018-01-30 18:06:15 +0800

    setting

---
+++ src/modules/setting/actions/spec/kpi.spec.ts
@@ -0,0 +1,36 @@
+// tslint:disable
+import { assert } from 'chai';
+import configureStore from 'redux-mock-store'
+import reduxThunk from 'redux-thunk';
+import { getTimeFilter, getPackages, getMoInstKpiThresholds } from '../kpi'
+const middlewares = [reduxThunk];
+const mockStore = configureStore(middlewares)
+
+declare const sinon;
+
+describe('Kpi actions', () => {
+    it('assert get timeFilter data', (done) => {
+        const store = mockStore({});
+        store.dispatch(getTimeFilter(null)).then(() => {
+            const actions = store.getActions()
+            assert.isNotNull(actions[0].timeFilter)
+            done()
+        })
+    })
+    it('assert get packages data', (done) => {
+        const store = mockStore({});
+        store.dispatch(getPackages(null)).then(() => {
+            const actions = store.getActions()
+            assert.isNotNull(actions[0].nfvdPm)
+            done()
+        })
+    })
+    it('assert get getMoInstKpiThresholds data', (done) => {
+        const store = mockStore({});
+        store.dispatch(getMoInstKpiThresholds(1, 1, null)).then(() => {
+            const actions = store.getActions()
+            assert.isNotNull(actions[0].moInstKpiThresholds)
+            done()
+        })
+    })
+})
---
+++ src/modules/setting/views/home.tsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import * as _ from 'lodash';
+import * as classNames from 'classnames';
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { matchPath } from 'react-router'
+import SplitPane from 'react-split-pane'
+import moment from '../../../common/moment'
+import { Row, Col, Breadcrumb, Icon, Tabs, Button } from 'antd';
+import FactModal from '../../../components/FactModal/'
+
+declare let global: any;
+
+import styles from '../style/index.less'
+
+class Home extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        let { match } = this.props
+        let { pathname } = this.props.location
+        this.state = {
+            activeKey: _.compact([
+                matchPath(pathname, { path: `${match.url}/current` }) != null && 'current',
+                matchPath(pathname, { path: `${match.url}/history` }) != null && 'history',
+            ]).toString(),
+            visible: false
+        };
+    }
+    triggerResize() {
+        let e: Event = document.createEvent('Event');
+        e.initEvent('resize', true, true);
+        window.dispatchEvent(e);
+    }
+    tabClick(e) {
+        let { match } = this.props
+        let path = e.target.getAttribute('data-path')
+        this.setState({
+            activeKey: path
+        })
+        global.hashHistory.push(`${match.url}/${path}`)
+    }
+    componentWillReceiveProps(nextProps) {
+        let { match } = nextProps
+        let { pathname } = nextProps.location
+        this.state = {
+            activeKey: _.compact([
+                matchPath(pathname, { path: `${match.url}/current` }) != null && 'current',
+                matchPath(pathname, { path: `${match.url}/history` }) != null && 'history',
+            ]).toString()
+        };
+    }
+    renderTab() {
+        let { activeKey } = this.state
+        let tab = [{ key: 'current', name: '当前状态' }, { key: 'history', name: '历史趋势' }]
+        return _.map(tab, (item) => {
+            let cls = {
+                tabItem: true,
+                active: activeKey === item.key ? true : false
+            }
+            return <li className={classNames(cls)} data-path={item.key} onClick={this.tabClick.bind(this)}>{item.name}</li>
+        })
+    }
+    showModal() {
+        this.setState({
+            visible: true
+        })
+    }
+    handleOk() {
+        this.setState({
+            visible: false
+        })
+    }
+    handleCancel() {
+        this.setState({
+            visible: false
+        })
+    }
+    componentWillMount() {
+    }
+    render() {
+        // console.log(`15分钟前:${moment().tz('Asia/Shanghai').subtract(15, 'minutes').format()}`)
+        // console.log(`开始时间:${moment().tz('Asia/Shanghai').subtract(15, 'minutes').valueOf()}`)
+        // console.log(`结束时间:${moment().tz('Asia/Shanghai').valueOf()}`)
+        let { match, tree } = this.props
+        let { activeKey } = this.state
+        // if (!tree) {
+        //     return <div>loading</div>
+        // }
+        return (
+            <Row className={styles.performance}>
+                <SplitPane
+                    split="vertical"
+                    minSize={100}
+                    maxSize={300}
+                    defaultSize={200}
+                    onChange={this.triggerResize} >
+                    <div>菜单树</div>
+                    <div className={styles.main}>
+                        <div className={styles.header}>
+                            <h1 className={styles.title}>交换机</h1>
+                            <Breadcrumb>
+                                <Breadcrumb.Item>性能监控</Breadcrumb.Item>
+                                <Breadcrumb.Item>二级菜单</Breadcrumb.Item>
+                                <Breadcrumb.Item>三级菜单</Breadcrumb.Item>
+                                <Breadcrumb.Item>四级菜单</Breadcrumb.Item>
+                            </Breadcrumb>
+                        </div>
+                        <div className={styles.tabBar}>
+                            <ul>
+                                {this.renderTab()}
+                            </ul>
+                            <Button onClick={this.showModal.bind(this)}><Icon type="tag-o" />添加指标</Button>
+                        </div>
+                        seting
+                    </div>
+                </SplitPane>
+
+            </Row>
+        );
+    }
+}
+
+export default Home;
