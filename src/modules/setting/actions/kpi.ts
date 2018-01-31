04bc078325da7a502bb44283f076cca5da650515
zhuxueyuan <865416263@qq.com>
2018-01-31 16:22:44 +0800

    setting userMock api

--- src/modules/setting/actions/kpi.ts
+++
@@ -1,4 +0,0 @@
-import * as _ from 'lodash'
-import ActionTypes from '../constants/actionTypes'
-import kpiAPI from '../api/kpiAPI'
-import { MatchingDimensionsParams, DataParams } from '../api/kpiAPI'
