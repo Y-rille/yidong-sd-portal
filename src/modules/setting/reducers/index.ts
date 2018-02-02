7881a97b00c9c775f9ecc508e501d9e26aa26f25
李继红 <707230595@qq.com>
2018-02-02 15:55:28 +0800

    删除reducer fix

--- src/modules/setting/reducers/index.ts
+++ src/modules/setting/reducers/index.ts
@@ -1,13 +1,11 @@
 import ActionTypes from '../constants/actionTypes'
 import * as SI from 'seamless-immutable';
 
-import * as _ from 'lodash';
-
 const SettingState = SI.from({
     name: 'RESOURCE',
     config: {},
     userList: null,
-    logList: null
+    logList: null,
 });
 
 let settingReducer = (state = SettingState, action = null) => {
@@ -15,7 +13,7 @@ let settingReducer = (state = SettingState, action = null) => {
         case ActionTypes.SETTING_SAY_HELLO:
             return state.merge(action, { deep: true })
         case ActionTypes.SETTING_DELETE_USER:
-            return state.setIn(['userList', 'rows'], _.values(SI.without(_.keyBy(state.userList.rows, 'id'), action.id)), { ddep: true });
+            return state.setIn(['userList', 'rows'], state.getIn(['userList', 'rows']).filter(o => o.id !== parseInt(action.id, 0)));
         default:
             return state
     }
