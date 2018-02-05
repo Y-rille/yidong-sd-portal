5ba9e6b66e2450fde609ccdc47a7b3309a2c9431
小志 <379658593@qq.com>
2018-02-05 14:48:33 +0800

    fix

--- src/components/SearchResultPanel/index.tsx
+++ src/components/SearchResultPanel/index.tsx
@@ -28,15 +28,17 @@ export default class SearchResultPanel extends React.Component<SearchResultPanel
             groups[tempPath].push(temp)
         });
 
-        _.forEach(groups, function (value, key) {
-            list.push({
-                title: key.split(','),
-                description: groups[key]
-            })
-        });
+        for (let key in groups) {
+            if (groups.hasOwnProperty(key)) {
+                list.push({
+                    title: key.split(','),
+                    description: groups[key]
+                })
+            }
+        }
 
         list = list.map(item => {
-            item.title = this.getTitle(item.title, data);
+            item.title = getTitle(item.title, data);
 
             return item;
         });
@@ -65,60 +67,25 @@ export default class SearchResultPanel extends React.Component<SearchResultPanel
         if (!result) {
             return null;
         }
-
-        groups[tempPath].push(temp)
-    });
-
-    for(let key in groups) {
-        if (groups.hasOwnProperty(key)) {
-            list.push({
-                title: key.split(','),
-                description: groups[key]
-            })
-        }
-    }
-
-    list = list.map(item => {
-        item.title = getTitle(item.title, data);
-
-        return item;
-    });
-
-    return list;
-};
-
-const getTitle = (list, data) => {
-    let title = [];
-
-    list.map(id => {
-        const temp = data.find(item => (item.nodeId === id));
-        title.push(temp.nodeLabel)
-    });
-
-    return title.join(' / ')
-};
-
-const ListDescription = ({ list }) => {
-    let description = [];
-
-    list.map(item => {
-        description.push(<a style={{ marginRight: '20px' }}>{item.nodeLabel}</a>)
-    });
-
-    return description;
-};
-
-const SearchResultPanel = ({ result }) => {
-    if (!result) {
-        return null;
-    }
-
-    return (
-        <div className={styles.warpper}>
-            <div className={styles.header}>
-                <p>搜索关键字：{result.searchValue}</p>
-                <p>共搜索到 {result.selectedKeys.length} 条数据</p>
+        return (
+            <div className={styles.warpper}>
+                <div className={styles.header}>
+                    <p>搜索关键字：{result.searchValue}</p >
+                    <p>共搜索到 {result.selectedKeys.length} 条数据</p >
+                </div>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={this.fmtData(result.selectedKeys, result.data)}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta
+                                title={item.title}
+                                description={this.ListDescription.bind(item.description)}
+                            />
+                        </List.Item>
+                    )}
+                />
             </div>
-            )
+        )
     }
 }
