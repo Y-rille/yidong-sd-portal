0a122409b3549c979f54fa6b03d91c7d33304a19
lijie <jiel@kailuzhe.cn>
2018-02-07 11:36:43 +0800

    新建dashboard

--- src/modules/resource/routes/index.tsx
+++ src/modules/resource/routes/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import * as PropTypes from 'prop-types';
 import Home from '../container/home'
+import Dashboard from '../container/dashboard'
+import Vim from '../container/vim/vim'
+// import Pim from '../container/pim/pim'
 import { injectAsyncReducer } from '../../../common/store';
 import {
     Switch,
     Route,
 } from 'react-router-dom'
 
-let {resourceReducer} = require('../reducers/index');
+let { resourceReducer } = require('../reducers/index');
 
 export interface RoutesProps {
     // store
@@ -20,15 +23,18 @@ export default class Routes extends React.Component<RoutesProps, any> {
     }
 
     componentWillMount() {
-        let {store} = this.context
+        let { store } = this.context
         injectAsyncReducer(store, 'resourceReducer', resourceReducer);
     }
 
     render() {
-        let {match} = this.props
+        let { match } = this.props
         return (
             <Switch>
-                <Route path={`${match.url}`} component={Home}/>
+                <Route path={`${match.url}/dashboard`} component={Dashboard} />
+                <Route path={`${match.url}/vim`} component={Vim} />
+                {/* <Route path={`${match.url}/pim`} component={Pim} /> */}
+                <Route path={`${match.url}`} component={Home} />
             </Switch>
         );
     }
