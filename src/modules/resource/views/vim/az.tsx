4d76b6283da1b23ff5a4c7e901dc07950d00589a
yangxd <496689176@qq.com>
2018-02-07 17:12:07 +0800

    az,host,virtual页面添加查询控件

--- src/modules/resource/views/vim/az.tsx
+++ src/modules/resource/views/vim/az.tsx
@@ -3,17 +3,39 @@ import * as _ from 'lodash';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { matchPath } from 'react-router'
 import AzInfo from '../../container/vim/azInfo'
-import { Row, Col, Breadcrumb, Icon, Tabs, Button, Spin } from 'antd';
+import { Row, Col, Breadcrumb, Icon, Tabs, Button, Spin, Select, Input } from 'antd';
 import styles from '../../style/index.less'
+const Option = Select.Option;
 class Az extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            AZInputValue: 'AZ名称',
+            AZSelectValue: '1'
+        }
     }
     goInfo = () => {
         this.props.history.push(`/resource/vim/1/az/info`)
     }
+    AZSelectChange(value) {
+        const { AZSelectValue } = this.state;
+        this.setState({
+            AZSelectValue: value
+        })
+    }
+    AZInputChange(value) {
+        const { AZInputValue } = this.state;
+        this.setState({
+            AZInputValue: value
+        })
+    }
+    handleClick() {
+        const { AZInputValue, AZSelectValue } = this.state;
+        // console.log(AZInputValue, AZSelectValue)
+    }
     render() {
-        let { match } = this.props
+        let { match } = this.props;
+        const { AZInputValue, AZSelectValue } = this.state;
         return (
             <div>
                 <div className={styles.header}>
@@ -26,7 +48,28 @@ class Az extends React.Component<any, any> {
                     </Breadcrumb>
                 </div>
                 <div className={styles.queryBar}>
-                    queryBar
+                    <div style={{ marginBottom: 20, marginTop: 20 }}>
+                        <Select
+                            value={AZSelectValue}
+                            onChange={this.AZSelectChange.bind(this)}
+                            style={{ width: 120, marginLeft: 10 }}>
+                            <Option value="1">K1</Option>
+                            <Option value="2">K2</Option>
+                            <Option value="3">K3</Option>
+                        </Select>
+                        <Input
+                            value={AZInputValue} type="text"
+                            onChange={this.AZInputChange.bind(this)}
+                            style={{ width: 120, marginLeft: 20 }}
+                        />
+                        <Button
+                            type="primary"
+                            style={{ marginLeft: 10 }}
+                            onClick={this.handleClick.bind(this)}
+                        >
+                            查询
+                        </Button>
+                    </div>
                 </div>
                 <Switch>
                     <Route path={`${match.url}/info`} component={AzInfo} />
--- src/modules/resource/views/vim/virtual.tsx
+++ src/modules/resource/views/vim/virtual.tsx
@@ -3,17 +3,56 @@ import * as _ from 'lodash';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { matchPath } from 'react-router'
 import VirtualInfo from '../../container/vim/virtualInfo'
-import { Row, Col, Breadcrumb, Icon, Tabs, Button, Spin } from 'antd';
+import { Row, Col, Breadcrumb, Icon, Tabs, Button, Spin, Select } from 'antd';
 import styles from '../../style/index.less'
+const Option = Select.Option;
 class Virtual extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            menuValue: '1',
+            secondMenuValue: '1',
+            thiredMenuValue: '1',
+            fourthMenuValue: '1'
+        }
     }
     goInfo = () => {
         this.props.history.push(`/resource/vim/1/virtual/info`)
     }
+    menuChange(value) {
+        const { menuValue } = this.state;
+        this.setState({
+            menuValue: value
+        })
+    }
+
+    secondMenuChange(value) {
+        const { secondMenuValue } = this.state;
+        this.setState({
+            secondMenuValue: value
+        })
+    }
+
+    thiredMenuChange(value) {
+        const { thiredMenuValue } = this.state;
+        this.setState({
+            thiredMenuValue: value
+        })
+    }
+
+    fourthMenuChange(value) {
+        const { fourthMenuValue } = this.state;
+        this.setState({
+            fourthMenuValue: value
+        })
+    }
+    handleClick() {
+        const { menuValue, secondMenuValue, thiredMenuValue, fourthMenuValue } = this.state;
+        // console.log("selectValue:", menuValue, secondMenuValue, thiredMenuValue)
+    }
     render() {
-        let { match } = this.props
+        let { match } = this.props;
+        const { menuValue, secondMenuValue, thiredMenuValue, fourthMenuValue } = this.state;
         return (
             <div>
                 <div className={styles.header}>
@@ -26,7 +65,51 @@ class Virtual extends React.Component<any, any> {
                     </Breadcrumb>
                 </div>
                 <div className={styles.queryBar}>
-                    queryBar
+                    <div style={{ marginBottom: 20, marginTop: 20, marginLeft: 20 }}>
+                        <Select
+                            value={menuValue}
+                            onChange={this.menuChange.bind(this)}
+                            style={{ width: 120 }}>
+                            <Option value="1">H1</Option>
+                            <Option value="2">H2</Option>
+                            <Option value="3">H3</Option>
+                        </Select>
+
+                        <Select
+                            value={secondMenuValue}
+                            onChange={this.secondMenuChange.bind(this)}
+                            style={{ width: 120, marginLeft: 10 }}>
+                            <Option value="1">K1</Option>
+                            <Option value="2">K2</Option>
+                            <Option value="3">K3</Option>
+                        </Select>
+
+                        <Select
+                            value={thiredMenuValue}
+                            onChange={this.thiredMenuChange.bind(this)}
+                            style={{ width: 120, marginLeft: 10 }}>
+                            <Option value="1">Z1</Option>
+                            <Option value="2">Z2</Option>
+                            <Option value="3">Z3</Option>
+                        </Select>
+
+                        <Select
+                            value={fourthMenuValue}
+                            onChange={this.fourthMenuChange.bind(this)}
+                            style={{ width: 120, marginLeft: 10 }}>
+                            <Option value="1">Z1</Option>
+                            <Option value="2">Z2</Option>
+                            <Option value="3">Z3</Option>
+                        </Select>
+
+                        <Button
+                            type="primary"
+                            style={{ marginLeft: 10 }}
+                            onClick={this.handleClick.bind(this)}
+                        >
+                            查询
+                </Button>
+                    </div>
                 </div>
                 <Switch>
                     <Route path={`${match.url}/info`} component={VirtualInfo} />
