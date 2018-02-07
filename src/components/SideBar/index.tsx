31c100ff7c85d8dffe227ed09153757f3e0ccf70
songxiaodong <alina_0212@163.com>
2018-02-07 17:44:12 +0800

    style

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -31,23 +31,22 @@ export default class SideBar extends React.PureComponent<SideBarProps, any> {
         return (
             <Menu
                 onClick={this.handleClick}
-                style={{ color: '#525459', height: window.innerHeight - 64 }}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
+                style={{ height: window.innerHeight - 64 }}
+                defaultOpenKeys={['sub2']}
                 mode="inline"
             >
-                <SubMenu key="dashboard" title={<span><Icon type="mail" /><span>概览</span></span>}></SubMenu>
+                <SubMenu key="dashboard" title={<span><Icon type="mail" />概览</span>}></SubMenu>
                 <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>资源结构组织</span></span>}>
-                    <Menu.Item key="vim/1/az" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>AZ管理</Menu.Item>
-                    <Menu.Item key="vim/1/ha" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>HA管理</Menu.Item>
-                    <Menu.Item key="vim/1/host" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>主机管理</Menu.Item>
-                    <Menu.Item key="vim/1/virtual" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>虚拟机管理</Menu.Item>
+                    <Menu.Item key="vim/1/az">AZ管理</Menu.Item>
+                    <Menu.Item key="vim/1/ha">HA管理</Menu.Item>
+                    <Menu.Item key="vim/1/host">主机管理</Menu.Item>
+                    <Menu.Item key="vim/1/virtual">虚拟机管理</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub4" title={<span><Icon type="setting" /><span>物理部署组织</span></span>}>
-                    <Menu.Item key="pim/1/server" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>服务器管理</Menu.Item>
-                    <Menu.Item key="pim/1/firewall" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>防火墙管理</Menu.Item>
-                    <Menu.Item key="pim/1/switchboard" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>交换机管理</Menu.Item>
-                    <Menu.Item key="pim/1/magnetic" style={{ color: 'rgba(0, 0, 0, 0.45)' }}>磁阵管理</Menu.Item>
+                    <Menu.Item key="pim/1/server">服务器管理</Menu.Item>
+                    <Menu.Item key="pim/1/firewall">防火墙管理</Menu.Item>
+                    <Menu.Item key="pim/1/switchboard">交换机管理</Menu.Item>
+                    <Menu.Item key="pim/1/magnetic">磁阵管理</Menu.Item>
                 </SubMenu>
             </Menu>
         )
