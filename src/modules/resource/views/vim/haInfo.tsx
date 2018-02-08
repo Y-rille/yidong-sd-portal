3fc27f371fd3dfb364a162f7c534c59f99cbf746
吴丹丹 <danw@kailuzhe.cn>
2018-02-08 19:13:11 +0800

    HZ详情页面布局

--- src/modules/resource/views/vim/haInfo.tsx
+++ src/modules/resource/views/vim/haInfo.tsx
@@ -1,13 +1,109 @@
 import * as React from 'react';
 import * as _ from 'lodash';
+import {
+    Breadcrumb,
+    Icon,
+    Tabs,
+    Row,
+    Col,
+    Select,
+    Button,
+    Input
+} from 'antd';
+const Option = Select.Option;
 import styles from '../../style/index.less'
 class HaInfo extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            HostInputValue: '',
+            HZSelectValue: 'AZ'
+        }
     }
-    render() {
+    handleClick() {
+        const {HostInputValue, HZSelectValue} = this.state;
+        // console.log(HostInputValue, HZSelectValue)
+    }
+    HZSelectChange(value) {
+        this.setState({
+            HZSelectValue: value
+        })
+    }
+    HostInputChange(e) {
+        this.setState({
+            HostInputValue: e.target.value 
+        })
+    }
+    renderTitle = (title) => {
         return (
-            <div>HaInfo</div>
+            <div className={styles.nodeTitle}>
+                <span className={styles.nodeTitle1}></span>
+                <span className={styles.nodeTitle2}>{title}</span>
+            </div>
+        )
+    }
+
+    render() {
+        const { HostInputValue, HZSelectValue } = this.state;
+        return (
+            <div>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>HA详情</h1>
+                    <Breadcrumb>
+                        <Breadcrumb.Item><Icon type="home"/></Breadcrumb.Item>
+                        <Breadcrumb.Item>资源管理</Breadcrumb.Item>
+                        <Breadcrumb.Item>资源组织机构</Breadcrumb.Item>
+                        <Breadcrumb.Item>HA管理</Breadcrumb.Item>
+                        <Breadcrumb.Item>HA详情</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+                <div style={{ padding: '20px'}}>
+                    <div>
+                        {this.renderTitle('基本信息')}
+                        <div className={styles.nodeInfo} style={{marginBottom: '20px'}}>
+                            <Row className={styles.nodeRow}>
+                                <Col span={8}>HA数:&nbsp;&nbsp;12312</Col>
+                                <Col span={8}>Host数:&nbsp;&nbsp;12312</Col>
+                                <Col span={8}></Col>
+                            </Row>
+                            <Row className={styles.nodeRow}>
+                                <Col span={8}>VCPU（未使用/总）:&nbsp;&nbsp;21GB/26GB</Col>
+                                <Col span={8}>内存（未使用/总）:&nbsp;&nbsp;21GB/26GB</Col>
+                                <Col span={8}>硬盘（未使用/总）:&nbsp;&nbsp;21GB/26GB</Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <div>
+                        {this.renderTitle('主机')}
+                        <div className={styles.queryBar}>
+                            <Input
+                                value={HostInputValue}
+                                type="text"
+                                placeholder="主机名称"
+                                onChange={this.HostInputChange.bind(this)}
+                                style={{
+                                width: 120,
+                                
+                            }}/>    
+                            <Select
+                                value={HZSelectValue}
+                                onChange={this.HZSelectChange.bind(this)}
+                                style={{width: 120, marginLeft: 10 }}>
+                                <Option value="region">Region</Option>
+                            </Select>
+                            
+                            <Button
+                                type="primary"
+                                style={{marginLeft: 10}}
+                                onClick={this.handleClick.bind(this)}>查询
+                            </Button>
+                        </div>
+                    </div>
+                    <div>
+                        table区域
+                    </div>
+                </div>
+            </div>
         );
     }
 }
