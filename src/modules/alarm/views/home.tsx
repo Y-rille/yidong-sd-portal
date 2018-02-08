d7e4c6eb764fa3759f012d853f68739b4829f940
王喆 <nasa127@gmail.com>
2018-02-08 17:17:22 +0800

    告警模块绑定登录用户id

--- src/modules/alarm/views/home.tsx
+++ src/modules/alarm/views/home.tsx
@@ -5,9 +5,12 @@ import { Button, Icon, Popover } from 'antd';
 
 class Home extends React.Component<any, any> {
     render() {
+        if (!this.props.currentUser) {
+            return <div />
+        }
         return (
             <div style={{ height: window.innerHeight - 64, width: window.innerWidth }}>
-                <iframe style={{ width: '100%', height: '100%' }} src="http://47.94.4.45:8080/alarm-mntr/flex/main.html?userId=admin"></iframe>
+                <iframe style={{ width: '100%', height: '100%' }} src={`http://47.94.4.45:8080/alarm-mntr/flex/main.html?userId=${this.props.currentUser.id}`}></iframe>
             </div>
         );
     }
