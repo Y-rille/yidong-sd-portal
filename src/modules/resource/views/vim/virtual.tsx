d4bdb9276abf624c528d60f474cabda7b35d5d1d
吴丹丹 <danw@kailuzhe.cn>
2018-02-11 14:01:12 +0800

    虚机列表传递table数据

--- src/modules/resource/views/vim/virtual.tsx
+++ src/modules/resource/views/vim/virtual.tsx
@@ -61,104 +61,114 @@ class Virtual extends React.Component<any, any> {
             'count': 17,
             'header': [{
                 key: 'id',
-                title: '编号',
+                title: '虚拟机名称',
                 fixed: true,
-                link: true,
             }, {
                 key: 'name',
-                title: '姓名',
+                title: '主机',
                 fixed: true,
             }, {
                 key: 'mobile',
-                title: '电话',
+                title: '项目',
             }, {
                 key: 'vm',
-                title: 'VM值'
+                title: '镜像'
             },
             {
                 key: 'email',
-                title: '邮箱',
+                title: 'IP地址',
             }, {
                 key: 'cpu',
-                title: 'CPU'
+                title: 'CPU数'
             }, {
                 key: 'memory',
-                title: '内存'
+                title: '状态'
             }, {
                 key: 'role',
-                title: '角色',
+                title: '运行时间',
             }],
             'body': [
                 {
-                    'id': 100077,
-                    'email': 'zhan21@hpe.com',
-                    'name': '张三21',
-                    'mobile': '15811001101',
-                    'cpu': '1/10',
-                    'memory': '50%',
-                    'role': '管理员',
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
                     'vm': 20
                 },
                 {
-                    'id': 100056,
-                    'email': 'dandan',
-                    'name': '用户1',
-                    'mobile': '13211111111',
-                    'cpu': '1/10',
-                    'memory': '70%',
-                    'role': '普通会员',
-                    'vm': 25
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
                 },
                 {
-                    'id': 100003,
-                    'email': 'admin@cmp.com',
-                    'name': '管理员',
-                    'mobile': '13211117890',
-                    'cpu': '1/10',
-                    'memory': '40%',
-                    'role': 'VIP',
-                    'vm': 15
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
                 },
                 {
-                    'id': 100005,
-                    'email': 'admin@cmp.com',
-                    'name': '会员6',
-                    'mobile': '13211127890',
-                    'cpu': '3/10',
-                    'memory': '55%',
-                    'role': '普通会员',
-                    'vm': 13
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
                 },
                 {
-                    'id': 100025,
-                    'email': 'admin3@cmp.com',
-                    'name': '会员1',
-                    'mobile': '13311127890',
-                    'cpu': '3/10',
-                    'memory': '65%',
-                    'role': '普通会员',
-                    'vm': 13
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
                 },
                 {
-                    'id': 100075,
-                    'email': 'admin3@cmp.com',
-                    'name': '会员8',
-                    'mobile': '13911127890',
-                    'cpu': '4/10',
-                    'memory': '25%',
-                    'role': '普通会员',
-                    'vm': 21
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
                 },
                 {
-                    'id': 100575,
-                    'email': 'admin8@cmp.com',
-                    'name': '会员10',
-                    'mobile': '18811127890',
-                    'cpu': '4/10',
-                    'memory': '45%',
-                    'role': '普通会员',
-                    'vm': 22
-                }
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
+                },
+                {
+                    'id': 'whj_train',
+                    'email': 'whj_train',
+                    'name': 'p3tenant_c119699c',
+                    'mobile': 'win2012',
+                    'cpu': 'HW-Volte-Test-Busi-V1175 188.103.19.171',
+                    'memory': '42134',
+                    'role': '14天24小时',
+                    'vm': 20
+                },
+                
             ]
         }
         let { match } = this.props;
