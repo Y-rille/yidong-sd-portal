f952fad0815a46f8fb3d68ca5bd421fbb5a5f65e
王曼玉 <manyu.wang@hpe.com>
2018-02-12 13:24:17 +0800

    NoData 样式调整

--- src/components/NoData/index.tsx
+++ src/components/NoData/index.tsx
@@ -6,13 +6,21 @@ export interface NoDataProps {
 }
 
 export default class NoData extends React.PureComponent<NoDataProps, any> {
-
+    container: any
+    componentDidMount() {
+        let mainH = window.innerHeight - 104 // document.getElementById('mainRight').offsetHeight
+        let minH = this.container.offsetTop
+        this.container.style.height = mainH - minH + 'px'
+    }
     render() {
         return (
-            <div className={styles.noData}>
-                <img src={require('../../img/noData.png')} alt="" />
-                <br />
-                <span>当前页面暂无相关数据！</span>
+            <div ref={(node) => { this.container = node }} className={styles.noData}>
+                <div>
+                    <img src={require('../../img/noData.png')} alt="" />
+                    <br />
+                    <span>当前页面暂无相关数据！</span>
+                </div>
+
             </div>
         )
     }
