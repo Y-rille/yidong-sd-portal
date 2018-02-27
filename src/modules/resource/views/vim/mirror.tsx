51108c749a996916f2fd375960be3f46869cf906
xiangxiang <smile78356@foxmail.com>
2018-02-27 17:15:28 +0800

    镜像、虚拟网络 list

--- src/modules/resource/views/vim/mirror.tsx
+++ src/modules/resource/views/vim/mirror.tsx
@@ -4,17 +4,170 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { matchPath } from 'react-router'
 // import MirrorInfo from '../../container/vim/mirrorInfo'
 import AzInfo from '../../container/vim/azInfo'
-import { Row, Col, Breadcrumb, Icon, Tabs, Button, Spin } from 'antd';
+import { Row, Col, Breadcrumb, Icon, Tabs, Button, Select, Input, Spin } from 'antd';
+const Option = Select.Option;
+import CompactTable from '../../../../components/CompactTable/'
 import styles from '../../style/index.less'
 class Mirror extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            menuValue: 'project'
+        }
     }
     goInfo = () => {
         this.props.history.push(`/resource/vim/1/mirror/info`)
     }
-    render() {
+    menuChange(value) {
+        this.setState({
+            menuValue: value
+        })
+    }
+    handleClick() {
+        const { menuValue } = this.state;
+        // console.log("selectValue:", menuValue)
+    }
+    goPage() {
+    }
+    goLink(key, obj) {
         let { match } = this.props
+        if (key === 'id') {
+            this.props.history.push(`${match.url}/info/${obj.id}`)
+        }
+    }
+    render() {
+        let tData = {
+            'count': 17,
+            'header': [
+                // {
+                //     key: 'id',
+                //     title: '虚拟机名称',
+                //     link: true,
+                //     fixed: true,
+                // },
+                {
+                    key: 'mobile',
+                    title: '项目',
+                }, {
+                    key: 'name',
+                    title: '名称',
+                }, {
+                    key: 'type',
+                    title: '类型'
+                }, {
+                    key: 'memory',
+                    title: '状态'
+                },
+                {
+                    key: 'cache',
+                    title: 'RAW cache',
+                }, {
+                    key: 'public',
+                    title: '是否公开'
+                }, {
+                    key: 'proteted',
+                    title: '是否proteted ',
+                },
+                {
+                    key: 'format',
+                    title: '格式',
+                }, {
+                    key: 'size',
+                    title: '大小（MB)'
+                }],
+            'body': [
+                {
+                    'mobile': 'xiaojindian4',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': '213cluster',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': 'xiaojindian4',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': '213cluster',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': 'xiaojindian4',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': '213cluster',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': 'xiaojindian4',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+                {
+                    'mobile': '213cluster',
+                    'name': 'cirros',
+                    'type': '镜像',
+                    'memory': '运行',
+                    'cache': '不可用',
+                    'public': 'Yes',
+                    'proteted': 'Yes',
+                    'format': 'QCOW2',
+                    'size': '18'
+                },
+            ]
+        }
+        let { match } = this.props
+        const { menuValue } = this.state;
         return (
             <Switch>
                 <Route path={`${match.url}/info/:id`} component={AzInfo} />
@@ -29,8 +182,35 @@ class Mirror extends React.Component<any, any> {
                                 <Breadcrumb.Item>镜像管理</Breadcrumb.Item>
                             </Breadcrumb>
                         </div>
-                        <div className={styles.queryBar}>
-                            queryBar
+                        <div style={{ padding: '20px' }}>
+                            <div className={styles.queryBar}>
+                                <Select
+                                    value={menuValue}
+                                    onChange={this.menuChange.bind(this)}
+                                >
+                                    <Option value="project">project</Option>
+                                </Select>
+                                <Input placeholder="镜像名称" />
+                                <Button
+                                    type="primary"
+                                    onClick={this.handleClick.bind(this)}
+                                >
+                                    查询
+                                </Button>
+                                <Button style={{ float: 'right' }}
+                                    type="primary"
+                                >
+                                    管理
+                            </Button>
+                            </div>
+                            <CompactTable
+                                outStyle={{ marginTop: '20px' }}
+                                goPage={this.goPage.bind(this)} // 翻页
+                                goLink={this.goLink.bind(this)}
+                                data={tData}
+                                pageAuth={true}
+                                actionAuth={[]}
+                            />
                         </div>
                     </div>
                 )} />
