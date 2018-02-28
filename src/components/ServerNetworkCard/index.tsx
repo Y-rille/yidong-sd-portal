b2fd40658fac05d8622cf3f7bb2021bd46e901cf
小志 <379658593@qq.com>
2018-02-28 17:38:07 +0800

    serverNetworkCard

--- src/components/ServerNetworkCard/index.tsx
+++ src/components/ServerNetworkCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './index.less';
+import CompactTable from '../CompactTable'
 
 export interface ServerNetworkCardProps {
 
@@ -14,11 +15,21 @@ class ServerNetworkCard extends React.PureComponent<ServerNetworkCardProps, any>
     render() {
         return (
             <div className={styles.serverNetworkCard}>
-                网卡信息
+                <div className={styles.title}>
+                    <span>网卡型号：</span><span className={styles.title_header}>p3tenant_c119699c-39cb-400d-8f06-6a85b31f7eb9</span>&emsp;
+                    <span>接口类型：</span><span className={styles.title_header}>whj_train</span>&emsp;
+                    <span>状态：</span><span className={styles.title_header}>ok</span>
+                </div>
+                <CompactTable
+                    // goPage={this.goPage.bind(this)} // 翻页
+                    // goLink={this.goLink.bind(this)}
+                    // data={null}
+                    actionAuth={['delete']}
+                    pageAuth={false}
+                />
             </div>
         )
     }
-
 }
 
 export default ServerNetworkCard;
