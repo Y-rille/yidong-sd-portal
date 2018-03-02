d185e401e2d0e14c5036b03e61aa3dac62e7f7b9
songxiaodong <alina_0212@163.com>
2018-03-02 17:46:49 +0800

    左侧导航对接口

--- src/modules/resource/views/home.tsx
+++ src/modules/resource/views/home.tsx
@@ -26,20 +26,21 @@ class Home extends React.Component<any, any> {
     handleClick = (key) => {
         this.props.history.push(`${key}`)
     }
-    componentDidMount() {
+    componentWillMount() {
         this.props.actions.getMoTree()
     }
+
     renderSider(match) {
         let route = this.props.location.pathname.replace('/resource/', '')
+        let resourceTree = this.props.resourceTree
         let current = route.replace('/info', '')
-        if (current.indexOf('resource') < 0) {
-            return <SideBar match={match} pathname={this.props.location.pathname} onLinkHandleClick={this.handleClick} />
+        if (resourceTree && current.indexOf('resource') < 0) {
+            return <SideBar match={match} pathname={this.props.location.pathname} onLinkHandleClick={this.handleClick} resourceTree={resourceTree} />
         } else {
             return <div />
         }
     }
     render() {
-        // console.log(this.props.resourceTree, '0000');
         let { match } = this.props
         return (
             <Row className={styles.resource}>
