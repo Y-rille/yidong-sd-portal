b7f1e4a3162945046ee73b7438a2ecd779d32fc5
lijie <jiel@kailuzhe.cn>
2018-03-07 10:58:18 +0800

    告警管理去掉横向滚动条

--- src/modules/alarm/views/home.tsx
+++ src/modules/alarm/views/home.tsx
@@ -9,7 +9,7 @@ class Home extends React.Component<any, any> {
             return <div />
         }
         return (
-            <div style={{ height: window.innerHeight - 64, width: window.innerWidth }}>
+            <div style={{ height: window.innerHeight - 64, width: '100%' }}>
                 <iframe style={{ width: '100%', height: '100%', border: '0px' }} src={`http://47.94.4.45:8080/alarm-mntr/flex/main.html?userId=${this.props.currentUser.id}`}></iframe>
             </div>
         );
