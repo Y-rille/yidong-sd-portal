03741a37f9ab7e3fa484ee0f440f3126953cca32
王喆 <nasa127@gmail.com>
2018-03-07 18:09:07 +0800

    example data

---
+++ example_data/三级联动示例数据.js
@@ -0,0 +1,25 @@
+[{
+  value: 'zhejiang',
+  label: 'Zhejiang',
+  children: [{
+    value: 'hangzhou',
+    label: 'Hangzhou',
+    children: [{
+      value: 'xihu',
+      label: 'West Lake',
+      code: 752100,
+    }],
+  }],
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+  children: [{
+    value: 'nanjing',
+    label: 'Nanjing',
+    children: [{
+      value: 'zhonghuamen',
+      label: 'Zhong Hua Men',
+      code: 453400,
+    }],
+  }],
+}]
