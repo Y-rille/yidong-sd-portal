d552415318ac250ce1568f3a622752d548e6b60f
吴丹丹 <danw@kailuzhe.cn>
2018-03-08 11:47:16 +0800

    数据中心的mock

--- mock/mo.js
+++ mock/mo.js
@@ -604,7 +604,79 @@ let morel = {
   }
 }
 
+/**
+ * 数据中心下拉选项树
+ */
+let dataTree = {
+  path: '/datashare-svr/api/querytree/dcMachineRoomCabinet',
+  method: 'GET',
+  cache: false,
+  template: (params, query, body) => {
+    return [
+      {
+        "nodeLabel": "数据中心1",
+        "nodeId": "1",
+        "nodeName": "数据中心1",
+        "nodeType": "DataCenter",
+        "labelPath": "数据中心1",
+        "dataType": 0,
+        "children": [
+          {
+            "nodeLabel": "机房1",
+            "nodeId": "1",
+            "nodeName": "机房1",
+            "nodeType": "MachineRoom",
+            "labelPath": "数据中心1/机房1",
+            "dataType": 0,
+            "children": [
+              {
+                "nodeLabel": "机柜1",
+                "nodeId": "1",
+                "nodeName": "机柜1",
+                "nodeType": "CABINET",
+                "labelPath": "数据中心1/机房1/机柜1",
+                "dataType": 0
+              }, {
+                "nodeLabel": "机柜2",
+                "nodeId": "2",
+                "nodeName": "机柜2",
+                "nodeType": "CABINET",
+                "labelPath": "数据中心1/机房1/机柜2",
+                "dataType": 0
 
+              }
+            ]
+          }, {
+            "nodeLabel": "机房2",
+            "nodeId": "2",
+            "nodeName": "机房2",
+            "nodeType": "MachineRoom",
+            "labelPath": "数据中心1/机房2",
+            "dataType": 0,
+            "children": [{}]
+          }
+        ]
+      }, {
+        "nodeLabel": "数据中心2",
+        "nodeId": "2",
+        "nodeName": "数据中心2",
+        "nodeType": "DataCenter",
+        "labelPath": "数据中心2",
+        "dataType": 0,
+        "children": [
+          {
+            "nodeLabel": "机房3",
+            "nodeId": "3",
+            "nodeName": "机房3",
+            "nodeType": "MachineRoom",
+            "labelPath": "数据中心2/机房3",
+            "dataType": 0
+          }
+        ]
+      }
+    ]
+  }
+}
 
 /** 
  * 资源模块树图查询
@@ -930,6 +1002,7 @@ module.exports = {
   querydata,
   moinst,
   morel,
+  dataTree,
   mgrmoTree,
   querytree,
   subData,
