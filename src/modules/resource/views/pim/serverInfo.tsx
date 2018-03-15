31f6428bb39541489415c8febb009966eabb29cd
zhuxueyuan <865416263@qq.com>
2018-03-15 14:30:20 +0800

    fix

--- src/modules/resource/views/pim/serverInfo.tsx
+++ src/modules/resource/views/pim/serverInfo.tsx
@@ -237,7 +237,7 @@ class ServerInfo extends React.Component<any, any> {
                         icon="dingding"
                         style={{ margin: '0px 10px 0px 0' }}
                         onClick={this.confirmUpOrDown}
-                    >{this.state.status === 1 ? '上电' : '下电'}</Button>
+                    >{this.state.status === 2 ? '上电' : '下电'}</Button>
                     <Button type="primary" style={{ margin: '0px 10px 0px 0' }} ghost icon="retweet"
                         onClick={this.confirmRest.bind(this, 'reset')}>复位</Button>
                     <Button type="primary" ghost icon="eye-o" onClick={this.goHost.bind(this)}>查看主机</Button>
