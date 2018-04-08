f152cb964dd0f4d2ebccdf5a603de7654665a3ad
xiangxiang <smile78356@foxmail.com>
2018-04-08 17:32:31 +0800

    虚拟网络详情 && 存储卷详情

--- src/modules/resource/views/vim/virtualNetworkInfo.tsx
+++ src/modules/resource/views/vim/virtualNetworkInfo.tsx
@@ -1,13 +1,107 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 import styles from '../../style/index.less'
+import { Breadcrumb, Icon, Tabs, Spin } from 'antd';
+import DynamicPropertiesCollapse from '../../../../components/DynamicPropertiesCollapse'
+import LogShine from '../../../../components/LogShine/'
+import fmtLog from '../../utils/fmtLog'
+const TabPane = Tabs.TabPane;
 class VirtualNetworkInfo extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            events: [],
+        }
+    }
+    onChange() {
+
+    }
+    goList() {
+        let path = this.props.location.pathname.replace(/\/info\/(\w+)/, '')
+        this.props.history.push(`${path}`)
+    }
+    tabInfo = (key) => {
+        this.setState({
+            showBtn: key === 'log' ? false : true
+        })
+        if (key === 'log') {
+            this.props.actions.getSyslog('vm', this.props.match.params.id, (data, err) => {
+                if (data.code === 1) {
+                    this.setState({
+                        events: fmtLog(data.log)
+                    })
+                }
+            })
+        }
+    }
+    componentWillMount() {
+        let moTypeKey = 'vm'
+        let match = this.props.match
+        let id = match.params.id
+        this.props.actions.getObjAttributes(moTypeKey)
+        this.props.actions.getObjData(moTypeKey, id)
+    }
+    componentWillUnmount() {
+        this.props.actions.resetSyslog();
+        this.props.actions.resetObjAttributes()
+        this.props.actions.resetObjData()
+    }
+    renderDynamicPropertiesCollapse() {
+        if (this.props.objAttributes && this.props.objData) {
+            return (
+                <DynamicPropertiesCollapse attributes={this.props.objAttributes}
+                    data={this.props.objData} />
+            )
+        } else {
+            return (
+                <div style={{ position: 'relative', padding: '50px' }}>
+                    <Spin />
+                </div>
+            )
+        }
     }
     render() {
+        let { nodeInfo } = this.props
+        let { events } = this.state
+        let labelPathArr = nodeInfo ? nodeInfo.labelPath.split('/') : []
         return (
-            <div>VirtualNetworkInfo</div>
+            <div>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>虚拟网络详情</h1>
+                    {nodeInfo ? (
+                        <Breadcrumb>
+                            <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item>
+                            <Breadcrumb.Item>资源管理</Breadcrumb.Item>
+                            {
+                                labelPathArr.map((item, index) => {
+                                    return <Breadcrumb.Item key={index}>{item}</Breadcrumb.Item>
+                                })
+                            }
+                            <Breadcrumb.Item><a onClick={this.goList.bind(this)}>虚拟网络管理</a></Breadcrumb.Item>
+                            <Breadcrumb.Item>虚拟网络详情</Breadcrumb.Item>
+                        </Breadcrumb>
+                    ) : ''}
+                </div>
+                <div style={{ padding: '20px' }}>
+                    <Tabs onChange={this.onChange.bind(this)} animated={false} type="card">
+                        <TabPane tab="资源详情" key="1">
+                            <Tabs
+                                defaultActiveKey="1"
+                                animated={false}
+                                size="small"
+                                onChange={this.tabInfo}
+                            >
+                                <TabPane tab="资源概况" key="overview">
+                                    {this.renderDynamicPropertiesCollapse()}
+                                </TabPane>
+                                <TabPane tab="日志" key="log">
+                                    <LogShine events={events} />
+                                </TabPane>
+                            </Tabs>
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </div>
         );
     }
 }
