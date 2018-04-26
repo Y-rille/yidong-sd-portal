c9a02f8fe476a3e965933d74b18e7f01a1f3d874
王喆 <nasa127@gmail.com>
2018-04-26 11:56:36 +0800

    fix(parsing):threshold_manage custom_inspection

--- src/modules/resource/views/home.tsx
+++ src/modules/resource/views/home.tsx
@@ -48,7 +48,7 @@ class Home extends React.Component<any, any> {
         const next_mp_node: any = matchPath(nextProps.location.pathname, {
             path: '/resource/:type/:id'
         })
-        if ((dshboard_mp_node.params.dashboard === 'dashboard' && next_mp_node) || (pre_mp_node && next_mp_node && (pre_mp_node.params.id !== next_mp_node.params.id))) {
+        if ((dshboard_mp_node && dshboard_mp_node.params && dshboard_mp_node.params.dashboard === 'dashboard' && next_mp_node) || (pre_mp_node && next_mp_node && (pre_mp_node.params.id !== next_mp_node.params.id))) {
             this.getNodeInfo(next_mp_node.params.id)
         }
     }
